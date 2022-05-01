03573f57f014f50f0fdae2d4827fa444f02c9832
yesol <leeyesol07@gmail.com>
2022-05-01 20:32:16 +0900

    게시글 테스트

--- src/main/java/com/yesol/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yesol/book/springboot/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package com.yesol.book.springboot.service.posts;
 
 import com.yesol.book.springboot.domain.posts.Posts;
 import com.yesol.book.springboot.domain.posts.PostsRepository;
+import com.yesol.book.springboot.web.dto.PostsListResponseDto;
 import com.yesol.book.springboot.web.dto.PostsResponseDto;
 import com.yesol.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yesol.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성
 @Service
@@ -30,6 +33,14 @@ public class PostsService {
         return  id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById (Long id){
 
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
@@ -37,4 +48,10 @@ public class PostsService {
         return  new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
---
+++ src/main/java/com/yesol/book/springboot/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.yesol.book.springboot.web;
+
+import com.yesol.book.springboot.service.posts.PostsService;
+
+import com.yesol.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+ private final PostsService postsService;
+
+    @GetMapping("/")  //->  src/main/resources/templates/index.mustache
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
